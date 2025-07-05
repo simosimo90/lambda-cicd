8058a5b4618197530fcae16dcb3669972033fb4a
simosimo90 <simone.terazzi@hotmail.it>
2025-07-05 14:17:55 +0200

    try again

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Ciao dal mio githubaction su lambda!')
+        'body': json.dumps('Ciao dal mio github actions!')
     }
