9cd6c3df705b20e4c413302706f19d6ff5d0feca
simosimo90 <simone.terazzi@hotmail.it>
2025-07-05 14:09:03 +0200

    test again

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Ciao a tutti dal mio CICD githubactions workflow!')
+        'body': json.dumps('Ciao dal mio githubaction su lambda!')
     }
