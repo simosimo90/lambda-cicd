48c7b2f586130bd2552516727e2827fac5125086
simosimo90 <simone.terazzi@hotmail.it>
2025-07-05 13:58:36 +0200

    updated lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CICD Lambda!')
+        'body': json.dumps('Ciao a tutti dal mio CICD githubactions workflow!')
     }
