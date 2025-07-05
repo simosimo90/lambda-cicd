ff997c41bf3c585dcd87f2e8f8bb3c815e43b500
simosimo90 <simone.terazzi@hotmail.it>
2025-07-05 16:31:17 +0200

    fixing the Deploy our Lambda Function job

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('cshsdshfd')
+        'body': json.dumps('Ciao dalla mia Lambda function triggherata con githubactions')
     }
